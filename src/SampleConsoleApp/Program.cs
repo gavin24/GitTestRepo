f41f9039ee71a27e120ba5d284ee63ced9023c2d
MAXXOR\jason.bennett <jason.bennett@maxxor.com>
2016-10-13 15:50:10 +0200

    test02

---
+++ src/SampleConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Multiply(2,2));
+        }
+
+        public static int Multiply(int one, int two)
+        {
+            return one + two;
+        }
+    }
+}
