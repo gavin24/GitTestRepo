749f05828e58ef83ae53fe7cca43a587756ccdfb
MAXXOR\jason.bennett <jason.bennett@maxxor.com>
2016-10-17 20:57:04 +0200

    test updates

--- src/SampleConsoleApp/Program.cs
+++ src/SampleConsoleApp/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SampleConsoleApp
 {
@@ -10,28 +7,21 @@ namespace SampleConsoleApp
         public static void Main(string[] args)
         {
             var mathLit = new MathLit();
-            Console.WriteLine(mathLit.ShowCalculation(2, 2));
-            Console.WriteLine(mathLit.ShowCalculation(2, 3));
-            Console.WriteLine(mathLit.ShowCalculation(2, 4));
+            Console.WriteLine(mathLit.ShowMultiplication(2, 2));
+            Console.WriteLine(mathLit.ShowMultiplication(2, 3));
+            Console.WriteLine(mathLit.ShowMultiplication(2, 4));
         }
 
         private class MathLit
         {
-            private int _one;
-            private int _two;
-
-            private int Multiply(int one, int two)
+            private static int Multiply(int one, int two)
             {
-                _one = one;
-                _two = two;
-                return _one + _two;
+                return one + two;
             }
 
-            public string ShowCalculation(int one, int two)
+            public string ShowMultiplication(int one, int two)
             {
-                _one = one;
-                _two = two;
-                return string.Format("{0} * {1} = {2}", _one, _two, Multiply(_one, _two));
+                return string.Format("{0} * {1} = {2}", one, two, Multiply(one, two));
             }
         }
     }
